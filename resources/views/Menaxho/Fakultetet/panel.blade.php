50439dc05fe9773c36c04d95027f6d9db536efbd
Florim <florimasani3@gmail.com>
2017-03-30 18:37:37 +0200

    seni ma i riu

--- resources/views/Menaxho/Fakultetet/panel.blade.php
+++ resources/views/Menaxho/Fakultetet/panel.blade.php
@@ -3,6 +3,7 @@
     Orari
 @stop
 
+
 @section('body')
 
     <!-- Modal Register-->
@@ -64,8 +65,20 @@
 
                         <div class="form-group">
                             {{ FORM::label('Foto',null,['class'=>'control-label col-md-4 col-sm-4 col-xs-12']) }}
+                            <div class="input-group image-preview control-label pull-right"><!-- don't give a name === doesn't send on POST/GET -->
+                            <span class="input-group-btn col-md-8 col-sm-8 col-xs-12">
+                                <!-- image-preview-clear button -->
+                                <button type="button" class="btn btn-default image-preview-clear" style="display:none;">
+                                    <span class="glyphicon glyphicon-remove"></span> Clear
+                                </button>
+                                <!-- image-preview-input -->
+                                <div class="btn btn-default image-preview-input">
+                                    <span class="glyphicon glyphicon-folder-open"></span>
+                                    <span class="image-preview-input-title">Browse</span>
                                     {!! FORM::file('photo') !!}
                                 </div>
+                            </span>
+                            </div><!-- /input-group image-preview [TO HERE]-->
                         </div>
                     </div>
                 </div>
