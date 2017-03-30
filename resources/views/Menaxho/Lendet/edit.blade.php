ee524de15587aaa590a13b0621edbfc7de034ad7
Florim <florimasani3@gmail.com>
2017-03-30 11:01:42 +0200

    Revert "Ma e reja"
    This reverts commit cbc540a6812ed60e8d953920b5f1eb15bfccd0ac.

---
+++ resources/views/Menaxho/Lendet/edit.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.master')
+@section('title')
+    Orari
+@stop
+@section('body')
+    <div class="x_panel">
+        <div class="x_title">
+            <h2>Modifiko Lëndën</h2>
+            <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+
+            <div class="col-md-6 col-md-offset-3">
+                {{ FORM::open(['class'=>'form-horizontal form-label-left input_mask','files'=>'true','url'=>'lendReg']) }}
+
+                <div class="form-group">
+                    {{ FORM::label('Emri',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ Form::select('age', ['Under 18', '19 to 30', 'Over 30']) }}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Emri',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::text('emri',null,['class'=>'form-control','required','placeholder'=>'Emri']) }}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Mbiemri',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::text('mbiemri',null,['class'=>'form-control','required','placeholder'=>'Mbiemri']) }}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Titulli',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::text('titulli',null,['class'=>'form-control','required','placeholder'=>'Titulli']) }}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Cakto Fakultetin:',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::text('dekan_i',null,['class'=>'form-control','required','placeholder'=>'Cakto Fakultetin:']) }}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Numri Personal',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::text('n_personal',null,['class'=>'form-control','required','placeholder'=>'Numri Personal']) }}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Passwordi',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::text('passwordi',null,['class'=>'form-control','required','placeholder'=>'Passwordi']) }}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Email',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::text('email',null,['class'=>'form-control','required','placeholder'=>'Email']) }}
+
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {{ FORM::label('Photo',null,['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) }}
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                        {{ FORM::label('file-upload',null,['class'=>'btn btn-info']) }}
+                        {{ FORM::file('img',['id'=>'file-upload','class'=>'form-control','onchange="readURL(this)"'])}}
+                        <br><br>
+                        <img id="img" src="#" />
+                    </div>
+
+                </div>
+
+                <div class="ln_solid"></div>
+                <div class="form-group">
+                    <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                        {{ FORM::submit('Regjistro',['class'=>'btn btn-success pull-right']) }}
+                    </div>
+                </div>
+            </div>
+            {{ FORM::close() }}
+
+        </div>
+    </div>
+@stop
